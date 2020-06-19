bb4b97cde2b1ed22ff07a2435c500448df444f84
Joshua Medlen <joshua246@gmail.com>
2020-06-18 19:19:14 -0500

    got the functions working git add src!

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -1,16 +1,31 @@
 const showBio = () => {
-  // const bioId = document.getElementById('#bioPage');
-  // const techId = document.getElementById('#techPage');
-  // const projectId = document.getElementById('#projectPage');
-  console.error('are we working');
-  if (document.getElementById('bioPage').class === 'hideBio') {
+  if (document.getElementById('bioPage').className === 'hideBio') {
     document.getElementById('bioPage').classList.remove('hideBio');
-    document.getElementById('bioPage').classList.add('showBio');
+    document.getElementById('techPage').classList.add('hideTech');
+    document.getElementById('project-page').classList.add('hideProject');
+  }
+};
+
+const showTech = () => {
+  if (document.getElementById('techPage').className === 'hideTech') {
+    document.getElementById('techPage').classList.remove('hideTech');
+    document.getElementById('bioPage').classList.add('hideBio');
+    document.getElementById('project-page').classList.add('hideProject');
+  }
+};
+
+const showProject = () => {
+  if (document.getElementById('project-page').className === 'hideProject') {
+    document.getElementById('project-page').classList.remove('hideProject');
+    document.getElementById('bioPage').classList.add('hideBio');
+    document.getElementById('techPage').classList.add('hideTech');
   }
 };
 
 const navbarEvents = () => {
   $('#aboutMe').click(showBio);
+  $('#techUsed').click(showTech);
+  $('#projectPage').click(showProject);
 };
 
 export default { navbarEvents };
