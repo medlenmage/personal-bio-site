b055bf1d51186763352f010a8db61753353c790c
Joshua Medlen <joshua246@gmail.com>
2020-08-15 13:34:41 -0500

    styled the projects to where they fit properly on page. added tech to tech page. updated about me with more information

--- src/javascripts/components/projectCards.js
+++ src/javascripts/components/projectCards.js
@@ -12,8 +12,10 @@ const createProjectCards = () => {
             <img class="projectScreenShot" rel="project screen shot" src="${project.screenshot}">
             <p class="projectDescription">${project.description}</p>
             <p class="techUsed">${project.technologiesUsed}</p>
-            <a class="url" href="${project.url}">${project.url}</a>
-            <a class="githubUrl" href="${project.githubUrl}">${project.githubUrl}</a>
+            <div class="links">
+              <a class="url" href="${project.url}">${project.url}</a>
+              <a class="githubUrl" href="${project.githubUrl}">${project.githubUrl}</a>
+            </div>
           </div>
       `;
       });
@@ -21,26 +23,5 @@ const createProjectCards = () => {
     })
     .catch((err) => console.error('get projects broke', err));
 };
-
-// const createProjectCards = () => {
-//   let domString = '';
-//   // const projectData = projects.getProjects();
-//   const projectData = axios.get(`${baseUrl}/projects.json}`);
-//   projectData();
-
-//   for (let i = 0; i < projectData.length; i += 1) {
-//     if (projectData[i].available === true) {
-//       domString += '<div class="projectCards">';
-//       domString += `<header class="projectTitle"><h4 class="projectTitleHeader">${projectData[i].title}</header>`;
-//       domString += `<img class="projectScreenShot" rel="project screen shot" src="${projectData[i].screenshot}">`;
-//       domString += `<p class="projectDescription">${projectData[i].description}</p>`;
-//       domString += `<p class="techUsed">${projectData[i].technologiesUsed}</p>`;
-//       domString += `<p class="url">${projectData[i].url}</p>`;
-//       domString += `<p class="githubUrl">${projectData[i].githubUrl}</p>`;
-//       domString += '</div>';
-//     }
-//   }
-//   print.printToDom('#projectsPage', domString);
-// };
 
 export default { createProjectCards };
